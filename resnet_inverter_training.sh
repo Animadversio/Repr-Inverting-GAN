7bf4b0b3cd7c2ef611be864461926c3499636810
BinxuWang <binxu.wang@wustl.edu>
2023-02-28 17:45:13 -0600

    add jobs to be submitted to O2 cluster

--- resnet_inverter_training.sh
+++ resnet_inverter_training.sh
@@ -4,7 +4,7 @@
 #SBATCH -t 12:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
-#SBATCH --array=4-6
+#SBATCH --array=7-9
 #SBATCH --mail-user=binxu_wang@hms.harvard.edu
 #SBATCH -o invert_resnet_%j.out
 
@@ -17,6 +17,9 @@ param_list=\
 --beta_l2 0.5 --beta_lpips 5.0 --lpips_net vgg --batch_size 64  --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_vgg_lpips5L205_B64
 --beta_l2 0.5 --beta_lpips 5.0 --lpips_net squeeze --batch_size 64  --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_squeeze_lpips5L205_B64
 --beta_l2 0.5 --beta_lpips 5.0 --lpips_net alex --batch_size 64  --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_alex_lpips5L205_B64
+--beta_l2 0.5 --beta_lpips 5.0 --gradclipnorm 10.0 --lpips_net vgg --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leaky_vgg_lpips5L205_gradclip
+--beta_l2 0.5 --beta_lpips 5.0 --gradclipnorm 10.0 --lpips_net squeeze --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leaky_squeeze_lpips5L205_gradclip
+--beta_l2 0.5 --beta_lpips 5.0 --gradclipnorm 10.0 --lpips_net alex --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leaky_alex_lpips5L205_gradclip
 '
 
 
