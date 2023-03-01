2fcb47153a28ab506cfa9873e9c06aa97a2797e5
BinxuWang <binxu.wang@wustl.edu>
2023-02-28 20:23:25 -0600

    add jobs

--- resnet_inverter_training.sh
+++ resnet_inverter_training.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -n 1
 #SBATCH -p gpu_quad
-#SBATCH -t 12:00:00
+#SBATCH -t 8:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
-#SBATCH --array=7-9
+#SBATCH --array=10-12
 #SBATCH --mail-user=binxu_wang@hms.harvard.edu
 #SBATCH -o invert_resnet_%j.out
 
@@ -20,6 +20,9 @@ param_list=\
 --beta_l2 0.5 --beta_lpips 5.0 --gradclipnorm 10.0 --lpips_net vgg --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leaky_vgg_lpips5L205_gradclip
 --beta_l2 0.5 --beta_lpips 5.0 --gradclipnorm 10.0 --lpips_net squeeze --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leaky_squeeze_lpips5L205_gradclip
 --beta_l2 0.5 --beta_lpips 5.0 --gradclipnorm 10.0 --lpips_net alex --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leaky_alex_lpips5L205_gradclip
+--beta_l2 0.5 --beta_lpips 2.0 --gradclipnorm 10.0 --lpips_net vgg --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leakyTanh_vgg_lpips2L205_gradclip
+--beta_l2 0.5 --beta_lpips 2.0 --gradclipnorm 10.0 --lpips_net squeeze --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leakyTanh_squeeze_lpips2L205_gradclip
+--beta_l2 0.5 --beta_lpips 2.0 --gradclipnorm 10.0 --lpips_net alex --batch_size 64 --leaky_relu_rgb 1 --blockclass Bottleneck_Up_Antichecker --runname run_antickbrd_2rgb_leakyTanh_alex_lpips2L205_gradclip
 '
 
 
